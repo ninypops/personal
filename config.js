49db15ee429c7880792222f70011f1fa59d8def7
Nina <nina@clicktech.com>
2020-10-09 12:17:49 +0100

    Update meta fields

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://ninypops.com',
   pathPrefix: '/',
   title: 'ninypops',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  subtitle: 'Front end developer, sharing her experiences in the tech industry and general life.',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: '',
   useKatex: false,
   menu: [
     {
